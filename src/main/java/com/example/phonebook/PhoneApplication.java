05c60a6eb4dbdf94d6fbd4c8b88a7512facb99ce
Andrei Shushakov <andrei.shushakov@tiger.trade>
2024-02-01 14:19:15 -0500

    moved a variable into the class scope

--- src/main/java/com/example/phonebook/PhoneApplication.java
+++ src/main/java/com/example/phonebook/PhoneApplication.java
@@ -6,9 +6,8 @@ import com.example.phonebook.service.ScannerInputProcessing;
 import java.util.Scanner;
 
 public class PhoneApplication {
+  private static final ScannerInputProcessing scanProccessing = new ScannerInputProcessing();
   public static void main(String[] args) {
-    private final ScannerInputProcessing scanProccessing = new ScannerInputProcessing();
-
     try(Scanner scanner = new Scanner(System.in)) {
       boolean isNotFinished = true;
 
