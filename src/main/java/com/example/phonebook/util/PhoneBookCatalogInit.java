846b147466b95fa0579b0359eab9cb8f4e795985
Andrei Shushakov <andrei.shushakov@tiger.trade>
2024-02-01 15:07:42 -0500

    added constructor, added modificator

--- src/main/java/com/example/phonebook/util/PhoneBookCatalogInit.java
+++ src/main/java/com/example/phonebook/util/PhoneBookCatalogInit.java
@@ -5,7 +5,7 @@ import java.util.TreeMap;
 
 public class PhoneBookCatalogInit {
 
-  private static Map<String, String> phoneBook = new TreeMap<>();
+  private static final Map<String, String> phoneBook = new TreeMap<>();
 
   static {
     phoneBook.put("Жека", "89312802629");
@@ -15,4 +15,6 @@ public class PhoneBookCatalogInit {
   public static Map<String, String> getPhoneBook() {
     return phoneBook;
   }
+
+  private PhoneBookCatalogInit(){};
 }
