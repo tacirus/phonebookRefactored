0e17a65a61a213aa10f47b655ac11afc183953ac
Andrei Shushakov <andrei.shushakov@tiger.trade>
2024-02-01 14:21:07 -0500

    added an empty line

--- src/main/java/com/example/phonebook/PhoneApplication.java
+++ src/main/java/com/example/phonebook/PhoneApplication.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 
 public class PhoneApplication {
   private static final ScannerInputProcessing scanProccessing = new ScannerInputProcessing();
+
   public static void main(String[] args) {
     try(Scanner scanner = new Scanner(System.in)) {
       boolean isNotFinished = true;
